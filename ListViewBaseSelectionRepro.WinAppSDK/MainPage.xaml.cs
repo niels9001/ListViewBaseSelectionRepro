c0efb2bb3c3042f956db95c7362a80e6089a5f0b
Niels Laute <niels.laute@live.nl>
2023-03-07 11:25:11 +0100

    Tweaked repro

--- ListViewBaseSelectionRepro.WinAppSDK/MainPage.xaml.cs
+++ ListViewBaseSelectionRepro.WinAppSDK/MainPage.xaml.cs
@@ -5,13 +5,24 @@ using Windows.UI.Xaml.Controls;
 using Microsoft.UI.Xaml;
 using Microsoft.UI.Xaml.Controls;
 #endif
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
 
 namespace ListViewBaseSelectionRepro;
 
 public sealed partial class MainPage : Page
 {
+    ObservableCollection<CustomListViewBaseItem> itemsCollection;
     public MainPage()
     {
         this.InitializeComponent();
+
+        itemsCollection = new ObservableCollection<CustomListViewBaseItem>
+        {
+            new CustomListViewBaseItem() { Content = "Item 1" },
+            new CustomListViewBaseItem() { Content = "Item 2" },
+            new CustomListViewBaseItem() { Content = "Item 3" },
+            new CustomListViewBaseItem() { Content = "Item 4" }
+        };
     }
 }
